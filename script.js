0eecf1c39c2d471f634bb769d755a400e7565912
Anvar Ganiev <60616540+99Anvar99@users.noreply.github.com>
2023-06-17 05:22:56 -0400

    Indent fixes
    - Lines where all around the place, indented and fixed
    
    - Please keep the code clean and easily readable

--- script.js
+++ script.js
@@ -42,50 +42,48 @@ async function searchMovies() {
          'X-RapidAPI-Host': 'spotify23.p.rapidapi.com'
      }
  };
- function album(){
-   musicContainer.innerHTML = "";
-     const url =  `https://spotify23.p.rapidapi.com/search/?q=%3C${search.value}%3E&type=multi&offset=0&limit=9&numberOfTopResults=9`;
-     fetch (url, options).then(function(response){
-         if(response.ok){
-             response.json()
-             .then(function(data){
-                 console.log(data);
-                 var albums = data.albums.items;
-                 console.log(albums);
-         
-                 for (var i = 0; i < albums.length; i++) {
-           
-                     var element = albums[i];
-                     var albumName = element.data.name;
-                     var artistName = element.data.artists.items[0].profile.name;
-                     var imgSrc = element.data.coverArt.sources[0].url;
-                     var albumTitle = albumName.Title;
-                      // Create a div for whole part
-                     var albumDiv = document.createElement("div");
-                     albumDiv.classList.add("album");
-                     // create a div for title
-                     var title = document.createElement("h2");
-                     title.textContent = albumName + "  :   "+  (artistName);
-                     albumDiv.appendChild(title);
-                     // get images
-                     var img = document.createElement("img");
-                     img.alt = albumTitle;
-                     img.src = imgSrc;
-                     // Link to spotify
-                     var linkElement = document.createElement("a");
-                    linkElement.href = element.data.uri;
-                    linkElement.appendChild(img);
-                     albumDiv.appendChild(linkElement);
-           musicContainer.appendChild(albumDiv);
-         }
-                
-               })
-             }
-         })
-     }
+function album() {
+  musicContainer.innerHTML = "";
+  const url = `https://spotify23.p.rapidapi.com/search/?q=%3C${search.value}%3E&type=multi&offset=0&limit=9&numberOfTopResults=9`;
+  fetch(url, options).then(function (response) {
+    if (response.ok) {
+      response.json()
+        .then(function (data) {
+          console.log(data);
+          var albums = data.albums.items;
+          console.log(albums);
+          for (var i = 0; i < albums.length; i++) {
+            var element = albums[i];
+            var albumName = element.data.name;
+            var artistName = element.data.artists.items[0].profile.name;
+            var imgSrc = element.data.coverArt.sources[0].url;
+            var albumTitle = albumName.Title;
+            // Create a div for whole part
+            var albumDiv = document.createElement("div");
+            albumDiv.classList.add("album");
+            // create a div for title
+            var title = document.createElement("h2");
+            title.textContent = albumName + "  :   " + (artistName);
+            albumDiv.appendChild(title);
+            // get images
+            var img = document.createElement("img");
+            img.alt = albumTitle;
+            img.src = imgSrc;
+            // Link to spotify
+            var linkElement = document.createElement("a");
+            linkElement.href = element.data.uri;
+            linkElement.appendChild(img);
+            albumDiv.appendChild(linkElement);
+            musicContainer.appendChild(albumDiv);
+          }
+
+        })
+    }
+  })
+}
  albumEl.addEventListener("click", album);
 
- 
+
   // event listener when clicking search
   searchbutton.addEventListener("click", searchMovies);
 
