4c684bf20fcc31d011211e8b46215d925ed637d9
HungQuocDang <129162404+HungQuocDang@users.noreply.github.com>
2023-06-18 11:09:24 -0400

    music js and html

--- music.js
+++ music.js
@@ -1,4 +1,7 @@
 // MUSIC
+
+var cardSongs=[];
+var cardSongsOuterLenght=0;
 var searchMusic = document.getElementById("searchMusic");
 var search = document.getElementById("search");
 
@@ -24,6 +27,8 @@ function album() {
                     console.log(data);
                     var albums = data.albums.items;
                     console.log(albums);
+                    var tableSongs=[];
+                    var cardSongs=[];
                     for (var i = 0; i < albums.length; i++) {
                         var element = albums[i];
                         var albumName = element.data.name;
@@ -37,6 +42,21 @@ function album() {
                         var title = document.createElement("h2");
                         title.textContent = albumName + "  :   " + (artistName);
                         albumDiv.appendChild(title);
+                    
+                        var setDiv= document.createElement("div");
+                        var likeBtnEl = document.createElement("button");
+                        likeBtnEl.setAttribute("data-song-index",i);             
+                        var spanEl = document.createElement("span");
+                        spanEl.textContent = " Song " + i;
+                        likeBtnEl.textcontent= "❤️";
+                        heartSymbol="❤️";
+                        likeBtnEl.addEventListener('click', heartTouch);
+                        setDiv.append(likeBtnEl);
+                        var linkElement = document.createElement("a");
+                        linkElement.textContent = "Link to Spotify";
+                        linkElement.href = element.data.uri;
+                        albumDiv.appendChild(linkElement);
+
                         // get images
                         var img = document.createElement("img");
                         img.alt = albumTitle;
@@ -47,7 +67,38 @@ function album() {
                         linkElement.appendChild(img);
                         albumDiv.appendChild(linkElement);
                         musicContainer.appendChild(albumDiv);
-                    }
+                        document.getElementById("musicContainer").appendChild(setDiv);
+                        tableSongs.push(albumName);
+                                        }
+                        var extractChrome=localStorage.getItem(`cardSongsB`);
+                        var arrayChrome=JSON.parse(extractChrome); 
+                            
+                        if (arrayChrome!=null ) {
+                        for (var k=0; k<arrayChrome.length; k++) {
+                        cardSongs.push(arrayChrome[k]);
+                        }
+                        }
+                        
+                    
+                        function heartTouch(event) {
+                            var songIndex = event.target.getAttribute("data-song-index"); 
+                            localStorage.setItem(`storeSongs`, albumName);
+                            localStorage.setItem(`songIndex`, songIndex);
+                            cardSongs.push(tableSongs[songIndex]);    
+                            localStorage.setItem(`cardSongsB`, JSON.stringify(cardSongs));
+                            cardSongsOuterLenght=(cardSongs.length);
+                        }  
+                        
+                        document.addEventListener("click", tableSavedSongs);
+                        function tableSavedSongs() {
+                        document.getElementById("demo").innerHTML = "SAVED songs:";
+                        for (let j=0; j<cardSongs.length; j++) { 
+                            var listCardSongs=document.createElement("p");
+                            listCardSongs.innerHTML=cardSongs[j];
+                            document.getElementById("demo").appendChild(listCardSongs);
+                        }
+
+                       }
 
                 })
         }
